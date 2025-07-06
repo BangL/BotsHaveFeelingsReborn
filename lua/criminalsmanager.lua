e8a1e3f14b324ca8facd0238c909998abc80ad32
BangL <henno.rickowski@gmail.com>
2025-07-06 18:54:49 +0200

    added 'competent bots' compat

--- lua/criminalsmanager.lua
+++ lua/criminalsmanager.lua
@@ -7,28 +7,30 @@ function CriminalsManager:init(...)
     if BotsHaveFeelingsReborn:GetConfigOption("improve_bot_aim") then
         local gang_weapon = tweak_data.character.presets.weapon.gang_member
         if gang_weapon then
-            for k, v in pairs(gang_weapon) do
-                v.focus_delay = 0
-                v.aim_delay = { 0, 0 }
-                v.range = tweak_data.character.presets.weapon.sniper.ger_kar98_npc.range
-                v.RELOAD_SPEED = 1
-                v.spread = 5
-                v.FALLOFF = {
-                    {
-                        r = 1500,
-                        acc = { 1, 1 },
-                        dmg_mul = 5,
-                        recoil = { 0.2, 0.2 },
-                        mode = { 0, 0, 0, 1 }
-                    },
-                    {
-                        r = 4500,
-                        acc = { 1, 1 },
-                        dmg_mul = 1,
-                        recoil = { 2, 2 },
-                        mode = { 0, 0, 0, 1 }
+            for _, v in pairs(gang_weapon) do
+                if (#v.FALLOFF == 2) and not (v.FALLOFF[1].dmg_mul == 7.5 and v.FALLOFF[2].dmg_mul == 7.5) then -- skip if 'competent bots' already did its job here
+                    v.focus_delay = 0
+                    v.aim_delay = { 0, 0 }
+                    v.range = tweak_data.character.presets.weapon.sniper.ger_kar98_npc.range
+                    v.RELOAD_SPEED = 1
+                    v.spread = 5
+                    v.FALLOFF = {
+                        {
+                            r = 1500,
+                            acc = { 1, 1 },
+                            dmg_mul = 5,
+                            recoil = { 0.2, 0.2 },
+                            mode = { 0, 0, 0, 1 }
+                        },
+                        {
+                            r = 4500,
+                            acc = { 1, 1 },
+                            dmg_mul = 1,
+                            recoil = { 2, 2 },
+                            mode = { 0, 0, 0, 1 }
+                        }
                     }
-                }
+                end
             end
         end
     end
